fc18461a2ad260b64846be376c9f118fd9bedb6d
Scott Fraser <scottfraserdev@gmail.com>
2019-03-19 13:00:56 -0700

    front end challenge built out

---
+++ front-end/index.js
@@ -0,0 +1,80 @@
+const $ = window.$;
+const listGroup = $(".list-group");
+
+let orderedList = [];
+let list = [
+  { name: "hawthorne-developer-evaluation.com", votes: 1 },
+  { name: "funky-cold-medina.com", votes: 4 },
+  { name: "timbers-rock.com", votes: 0 },
+  { name: "man-bear-pig.com", votes: 6 },
+  { name: "random-ecommerce-website.com", votes: 6 },
+  { name: "another-one.inc", votes: 10 },
+  { name: "best-website-ever.inc", votes: 10 },
+  { name: "another-one.inc", votes: 10 },
+  { name: "another-one.inc", votes: 10 },
+  { name: "another-one.inc", votes: 10 }
+];
+
+// this sorts by votes
+sortList = () => {
+  orderedList = list.sort((a, b) => parseFloat(b.votes - parseFloat(a.votes)));
+};
+
+// rerenders new list on vote
+makeList = () => {
+  listGroup.empty();
+  sortList();
+  orderedList.forEach(item => {
+    listGroup.append(`
+    <li
+    class="list-group-item d-flex justify-content-between align-items-center"
+  >
+    <span>
+      <span class="badge badge-primary badge-pill">${item.votes}</span>
+      ${item.name} 
+      </span>
+    </span>
+    <span>
+      <button
+        onclick="{handleUpVote(event)}"
+        type="button"
+        class="btn btn-primary"
+      >
+        +
+      </button>
+      <button
+        onclick="{handleDownVote(event)}"
+        type="button"
+        class="btn btn-primary"
+      >
+        -
+      </button>
+    </span>
+   </li>
+    `);
+  });
+};
+
+handleUpVote = e => {
+  const text = e.target.closest("li").innerText;
+  list.forEach(item => {
+    if (text.includes(item.name)) {
+      item.votes++;
+    }
+  });
+  makeList();
+};
+
+handleDownVote = e => {
+  const text = e.target.closest("li").innerText;
+  list.forEach(item => {
+    if (text.includes(item.name)) {
+      item.votes--;
+    }
+  });
+  makeList();
+};
+
+$(document).ready(() => {
+  makeList();
+});
